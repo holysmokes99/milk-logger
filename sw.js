c6fdb0a6cf352510543fae1f116765e7c5d94bea
holysmokes99 <marcushbell@gmail.com>
2026-08-21 15:23:57 -0700

    j

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // If offline, falls back to cached version.
 // When a new version is detected, activates immediately on next open.
 
-const CACHE_NAME = 'milk-logger-v1.0.40';
+const CACHE_NAME = 'milk-logger-v1.0.43';
 
 const FILES_TO_CACHE = [
   '/milk-logger/milk-logger.html',
